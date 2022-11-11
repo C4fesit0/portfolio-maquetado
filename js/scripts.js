aaade57c55e71def84ef543923f08d7dd0411ce0
Alan Lazo <al4nlaz0@gmail.com>
2022-11-11 12:41:25 -0300

    added: es and en text

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,31 @@ const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
 const toggleColors = document.getElementById('toggle-colors');
+
 const rootStyle = document.documentElement.style; //Aca es donde tenemos todas las variables del archivo css
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll('[data-section]')
+
+
+const changeLanguage = async language=>{
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        textToChange.innerHTML = texts[section][value]
+    }
+    
+}
+
+
+
+flagsElement.addEventListener("click", (e)=>{
+    changeLanguage(e.target.parentElement.dataset.language);
+})
 
 toggleTheme.addEventListener( "click",()=>{
     document.body.classList.toggle("dark");
